92de4c035a3340de71ded8570eb48843019365d0
Rohith <praroh2@gmail.com>
2019-12-25 23:49:48 +0530

    Populate skills section and add logos

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -2,11 +2,31 @@
   <div class="skills">
     <h2 class="skills__heading">Skills</h2>
     <h3 class="skills__subheading">Languages</h3>
-    <div class="row">
-      <div class="skills__languages col-xs-6 col-lg-3" v-for="language in languages" v-bind:key="language.name">
+    <div class="row skills__section">
+      <div class="col-xs-12 col-sm-6 col-lg-4 col-xl-2" v-for="language in languages" v-bind:key="language.name">
         <SkillCard
           :title=language.name
           :image=language.logo
+        >
+        </SkillCard>
+      </div>
+    </div>
+    <h3 class="skills__subheading u-margin-top-md">Environments, Tools and Services</h3>
+    <div class="row skills__section">
+      <div class="col-xs-12 col-sm-6 col-lg-4 col-xl-2" v-for="tool in tools" v-bind:key="tool.name">
+        <SkillCard
+          :title=tool.name
+          :image=tool.logo
+        >
+        </SkillCard>
+      </div>
+    </div>
+    <h3 class="skills__subheading u-margin-top-md">Libraries &amp; Frameworks</h3>
+    <div class="row skills__section">
+      <div class="col-xs-12 col-sm-6 col-lg-4 col-xl-2" v-for="framework in frameworks" v-bind:key="framework.name">
+        <SkillCard
+          :title=framework.name
+          :image=framework.logo
         >
         </SkillCard>
       </div>
@@ -31,22 +51,66 @@ export default {
       name: 'JavaScript',
       logo: 'images/javascript.svg',
     }, {
-      name: 'SQL',
-      logo: 'images/db.png'
-    }, {
       name: 'HTML',
       logo: 'images/html5.svg',
     }, {
       name: 'CSS',
       logo: 'images/css3.png',
     }, {
-      name: 'Sass',
-      logo: 'images/sass.svg',
+      name: 'SQL',
+      logo: 'images/db.png'
     }, {
       name: 'C',
       logo: 'images/c.svg'
     }]
-    const skills = { languages }
+    const tools = [{
+      name: 'Linux',
+      logo: 'images/linux.png',
+    }, {
+      name: 'Amazon Web Services',
+      logo: 'images/aws.png',
+    }]
+    const frameworks = [{
+      name: 'Django',
+      logo: 'images/dj.png',
+    }, {
+      name: 'Serverless',
+      logo: 'images/serverless.png',
+    }, {
+      name: 'Vue.js',
+      logo: 'images/vue.png',
+    }, {
+      name: 'Sass',
+      logo: 'images/sass.svg',
+    }, {
+      name: 'Flask',
+      logo: 'images/flask.svg',
+    }, {
+      name: 'Node.js',
+      logo: 'images/node.png',
+    }, {
+      name: 'Bootstrap',
+      logo: 'images/bootstrap.png',
+    }, {
+      name: 'Semantic UI',
+      logo: 'images/semantic.svg',
+    }, {
+      name: 'SQLAlchemy',
+      logo: 'images/sqla.png',
+    }, {
+      name: 'Beautiful Soup',
+      logo: 'images/pypi.svg',
+    }, {
+      name: 'Knockout.js',
+      logo: 'images/knockout.png',
+    }, {
+      name: 'jQuery',
+      logo: 'images/jquery.svg',
+    }, {
+      name: 'OpenAPI',
+      logo: 'images/openapi.png',
+    },]
+    const skills = { languages, tools, frameworks }
     return skills
   }
 }
