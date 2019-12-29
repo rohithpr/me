176ab02a364549e37eec44358f43c0b871506cd3
Rohith <praroh2@gmail.com>
2019-12-29 18:46:58 +0530

    Add experience section + refactor section resets

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="container-fluid">
       <Introduction></Introduction>
       <Skills></Skills>
+      <Experience></Experience>
     </div>
   </div>
 </template>
@@ -10,12 +11,14 @@
 <script>
 import Introduction from './components/introduction';
 import Skills from './components/skills'
+import Experience from './components/experience'
 
 export default {
   name: 'app',
   components: {
     Introduction,
     Skills,
+    Experience,
   }
 }
 </script>
--- src/components/skillSection.vue
+++ src/components/skillSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="skill-section">
-    <h3 class="skill-section__heading">{{ heading }}</h3>
+    <h3 class="skill-section__subheading">{{ heading }}</h3>
     <div class="row skill-section__list">
       <div class="col-xl-1 col-lg-1 col-md-2 col-sm-3 col-4" v-for="skill in skills" v-bind:key="skill.name">
         <SkillCard
